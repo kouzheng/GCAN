41bca47333c228f7e0eceddfbef0eaeae05bb7c7
kouzheng <46306687+kouzheng@users.noreply.github.com>
2025-09-16 23:45:46 +0800

    Update model.py

--- model.py
+++ model.py
@@ -34,38 +34,49 @@ from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, f1_s
 class GCAConv(MessagePassing):
     _alpha: OptTensor
     def __init__(self, in_channels: int, out_channels: int, heads: int,
-                 positional_channels:int = 15,
+                 geneflow_channels:int = 15,
                  concat: bool = True, negative_slope: float = 0.2,
                  dropout: float = 0., add_self_loops: bool = True,
                  bias: bool = True, share_weights: bool = False, **kwargs):
         super(GCAConv, self).__init__(node_dim=0, **kwargs)
+
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.positional_channels = positional_channels
+        self.geneflow_channels = geneflow_channels
         self.heads = heads
         self.concat = concat
         self.negative_slope = negative_slope
         self.dropout = dropout
         self.add_self_loops = add_self_loops
         self.share_weights = share_weights
+        # x
         self.lin_l = Linear(in_channels, heads * out_channels, bias=bias,
                             weight_initializer='glorot')
+        
         if share_weights:
             self.lin_r = self.lin_l
         else:
             self.lin_r = Linear(in_channels, heads * out_channels, bias=bias,
                                 weight_initializer='glorot')
+        
+        
+
         self.att = Parameter(torch.Tensor(1, heads, out_channels))
-        self.att2 = Parameter(torch.Tensor(1, heads, self.positional_channels))
+
+        self.att2 = Parameter(torch.Tensor(1, heads, self.geneflow_channels))
+
         if bias and concat:
             self.bias = Parameter(torch.Tensor(heads * out_channels))
         elif bias and not concat:
             self.bias = Parameter(torch.Tensor(out_channels))
         else:
             self.register_parameter('bias', None)
+
         self._alpha1 = None
         self._alpha2 = None
-        self.bias2 =  Parameter(torch.Tensor(self.positional_channels))
+
+        self.bias2 =  Parameter(torch.Tensor(self.geneflow_channels))
+
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -77,7 +88,7 @@ class GCAConv(MessagePassing):
         zeros(self.bias2)
 
     def forward(self, x: Union[Tensor, PairTensor], edge_index: Adj,
-                positional: Tensor,
+                geneflow: Tensor,
                 size: Size = None, return_attention_weights: bool = None):
         # type: (Union[Tensor, PairTensor], Tensor , Tensor, Size, NoneType) -> Tensor  # noqa
         # type: (Union[Tensor, PairTensor], SparseTensor, Size, NoneType) -> Tensor  # noqa
@@ -91,6 +102,7 @@ class GCAConv(MessagePassing):
                 attention weights for each edge. (default: :obj:`None`)
         """
         H, C = self.heads, self.out_channels
+
         x_l: OptTensor = None
         x_r: OptTensor = None
         if isinstance(x, Tensor):
@@ -100,12 +112,19 @@ class GCAConv(MessagePassing):
                 x_r = x_l
             else:
                 x_r = self.lin_r(x).view(-1, H, C)
+
+
         assert x_l is not None
         assert x_r is not None
-        positional = positional.unsqueeze(dim = 1)
-        positional = positional.repeat(1, self.heads, 1)
-        x_l = torch.cat((x_l,positional), dim = -1)
-        x_r = torch.cat((x_r,positional), dim = -1)
+        # print("geneflow", geneflow.shape)
+        geneflow = geneflow.unsqueeze(dim = 1)
+        # print("geneflow_sq", geneflow.shape)
+        geneflow = geneflow.repeat(1, self.heads, 1)
+        # print("geneflow", geneflow.shape)
+        # print("x_l", x_l.shape)
+        x_l = torch.cat((x_l,geneflow), dim = -1)
+        x_r = torch.cat((x_r,geneflow), dim = -1)
+
         if self.add_self_loops:
             if isinstance(edge_index, Tensor):
                 num_nodes = x_l.size(0)
@@ -117,19 +136,30 @@ class GCAConv(MessagePassing):
                 edge_index, _ = add_self_loops(edge_index, num_nodes=num_nodes)
             elif isinstance(edge_index, SparseTensor):
                 edge_index = set_diag(edge_index)
+
+        # propagate_type: (x: PairTensor)
         out_all = self.propagate(edge_index, x=(x_l, x_r), size=size)
+        # print("out_all",out_all.shape)
+
         out = out_all[ : , : , :self.out_channels ]
+
         out2 = out_all[ : , : , self.out_channels:]
+
         alpha1 = self._alpha1
         self._alpha1 = None
+
         alpha2 = self._alpha2
         self._alpha2 = None
+
         if self.concat:
             out = out.reshape(-1, self.heads * self.out_channels)
+            # print("outv2",out.shape)
         else:
             out = out.mean(dim=1)
+
         if self.bias is not None:
             out += self.bias
+
         out2 = out2.mean(dim=1)
         out2 += self.bias2
 
@@ -145,16 +175,23 @@ class GCAConv(MessagePassing):
     def message(self, x_j: Tensor, x_i: Tensor, index: Tensor, ptr: OptTensor,
                 size_i: Optional[int]) -> Tensor:
         x = x_i + x_j
-        x = F.leaky_relu(x, self.negative_slope)
+
         alpha1 = (x[:, :, :self.out_channels] * self.att).sum(dim=-1)
         alpha2 = (x[:, :, self.out_channels:] * self.att2).sum(dim=-1)
+
+        alpha1 = F.leaky_relu(alpha1 ,self.negative_slope )
+        alpha2 = F.leaky_relu(alpha2 ,self.negative_slope )
+
         alpha1 = softmax(alpha1, index, ptr, size_i)
         alpha2 = softmax(alpha2, index, ptr, size_i)
+        
         self._alpha1 = alpha1
         self._alpha2 = alpha2
         alpha1= F.dropout(alpha1, p=self.dropout, training=self.training)
         alpha2= F.dropout(alpha2, p=self.dropout, training=self.training)
+        # print("result",torch.cat((x_j[:, :, :self.out_channels]* alpha2.unsqueeze(-1), x_j[:, :, self.out_channels: ]* alpha1.unsqueeze(-1)) ,dim = -1).shape )
         return torch.cat((x_j[:, :, :self.out_channels]* alpha2.unsqueeze(-1), x_j[:, :, self.out_channels: ]* alpha1.unsqueeze(-1)) ,dim = -1)
+
 
     def __repr__(self):
         return '{}({}, {}, heads={})'.format(self.__class__.__name__,
@@ -163,127 +200,120 @@ class GCAConv(MessagePassing):
 
 
 class GCAN(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads , positional_hidden):
+    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads , geneflow_hidden):
         super(GCAN, self).__init__() 
         self.dropout = dropout
         self.alpha = alpha
-        self.positional_out = positional_hidden
+        self.geneflow_out = geneflow_hidden
         self.cj = nn.Conv1d(in_channels=4, out_channels=1 , kernel_size=1)
         self.cj_2 = nn.Conv1d(in_channels=1, out_channels=1 , kernel_size=15 , stride=15)
-        self.fc1 = nn.Linear(64, self.positional_out)
-        self.fc2 = nn.Linear(self.positional_out, 1)
+        self.fc1 = nn.Linear(64, self.geneflow_out)
+        self.fc2 = nn.Linear(self.geneflow_out, 1)
         self.conv1 = GCAConv(in_channels = 1000,
                                 out_channels = nhid,
                                 heads = nheads,
                                 dropout = self.dropout,
                                 negative_slope = self.alpha,
-                                positional_channels = self.positional_out
+                                geneflow_channels = self.geneflow_out,
+                                share_weights = True
                                 )
         self.conv2 = GCAConv(in_channels = nheads * nhid,
                                 out_channels = 1,
                                 heads = 1,
                                 dropout = self.dropout,
                                 negative_slope = self.alpha,
-                                positional_channels = 1
+                                geneflow_channels = 1,
+                                share_weights = True
                                 )
-    def forward(self, x ,  edge_index, positional):
+    def forward(self, x ,  edge_index, geneflow):
         x = self.cj(x)
         x = self.cj_2(x)
-        x = x.reshape(11374,-1)
+        x = x.reshape(x.shape[0],-1)
         x = F.dropout(x, p = self.dropout , training=self.training)
-        positional = F.dropout(positional, p = self.dropout , training=self.training)
-        positional = self.fc1(positional)
-        x , positional = self.conv1(x, edge_index , positional)
+        geneflow = F.dropout(geneflow, p = self.dropout , training=self.training)
+        geneflow = self.fc1(geneflow)
+        x , geneflow = self.conv1(x, edge_index , geneflow)
         x = F.relu(x) 
-        positional = F.relu(positional)
+        geneflow = F.relu(geneflow)
         x = F.dropout(x, p = self.dropout , training=self.training)
-        positional = F.dropout(positional, p = self.dropout , training=self.training)
-        positional = self.fc2(positional)
-        x ,positional = self.conv2(x, edge_index , positional) 
-        positional = positional.reshape(-1)
+        geneflow = F.dropout(geneflow, p = self.dropout , training=self.training)
+        geneflow = self.fc2(geneflow)
+        x ,geneflow = self.conv2(x, edge_index , geneflow) 
+        geneflow = geneflow.reshape(-1)
         x = x.reshape(-1)
-        x = torch.sigmoid(positional+x)
-        return x
-
-    def forward_conv1(self, x, edge_index, positional):
-        x = self.cj(x)
-        x = self.cj_2(x)
-        x = x.reshape(11374,x.shape[-1])
-
-        positional = F.dropout(positional, p = self.dropout , training=self.training)
-        positional = self.fc1(positional)
-        x = F.dropout(x, p = self.dropout , training=self.training)
-        x , positional = self.conv1(x, edge_index , positional)
+        x = torch.sigmoid(geneflow+x)
         return x
 
 
-data = np.loadtxt('data/deepwalk_64')
-T = data
-print(T.shape)
-tt=torch.tensor(T)
-positional = tt.float()
-
-positional= F.normalize(positional ,dim = 0)
-positional = positional.to(torch.float32)
-from sklearn.metrics import roc_curve, auc
+geneflow = torch.tensor(np.loadtxt('data/deepwalk_64')).float()
+geneflow= F.normalize(geneflow ,dim = 0).to(torch.float32)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-positional= positional.to(device)
+geneflow= geneflow.to(device)
+
 def train(epoch):
     # trian
     model.train()
     optimizer.zero_grad()
-    output = model(features, edge_index , positional)
-
-    weights = torch.tensor([1.0,10.0]).to(device)
+    output = model(features, edge_index , geneflow)
     loss_train = F.binary_cross_entropy(output[idx_train], labels[idx_train].float() )
     loss_train.backward()
     optimizer.step()
     # eval
     model.eval()
-    output = model(features, edge_index , positional)
+    with torch.no_grad():
+        output = model(features, edge_index , geneflow)
     loss_val = F.binary_cross_entropy(output[idx_val], labels[idx_val].float())
-    with open("loss_record.txt","a") as f:      
-            f.write("\n"+"loss_train:"+str(loss_train.data.item())+
-                    "\n"+"loss_val :"+str(loss_val.data.item())) 
     return loss_val.data.item()
 
-from sklearn.metrics import precision_recall_curve, auc
+from sklearn.metrics import (
+    roc_curve, roc_auc_score, precision_recall_curve, average_precision_score,
+    precision_score, recall_score, f1_score, confusion_matrix, mean_squared_error , auc
+)
+
 def compute_test():
     model.eval()
+    with torch.no_grad():
+        output = model(features,edge_index , geneflow)
+    y_true = labels[idx_test].cpu().numpy()
+    y_score = output[idx_test].cpu().detach().numpy()
 
-    output = model(features,edge_index , positional)
-    y_true  = labels[idx_test].cpu()
-
-    fpr, tpr, thresholds = roc_curve(y_true, output[idx_test].cpu().detach())
+    fpr, tpr, thresholds = roc_curve(y_true, y_score)
     roc_auc = auc(fpr, tpr)
-    
-    # Find the optimal threshold by finding the point with the maximum true positive rate minus false positive rate
+
+
     optimal_idx = np.argmax(tpr - fpr)
     optimal_threshold = thresholds[optimal_idx]
-    optimal_threshold
 
-    precision, recall, thresholds = precision_recall_curve(y_true,output[idx_test].cpu().detach())
+    precision, recall, thresholds = precision_recall_curve(y_true, y_score)
     aupr = auc(recall, precision)
 
-    acc_test = accuracy(output[idx_test], labels[idx_test]  ,  optimal_threshold)
-    print(acc_test.item())
-    print('optimal_threshold:'+  str(optimal_threshold))
-    y_pred = (output > optimal_threshold).type_as(labels)[idx_test].cpu()
+
+    acc_test = accuracy(output[idx_test], labels[idx_test], optimal_threshold)
+
+    print('optimal_threshold:' + str(optimal_threshold))
+
+
+    y_pred = (y_score > optimal_threshold).astype(int)
     assert len(y_true) == len(y_pred)
-    precision = precision_score(y_true, y_pred)
+
+
+    precision_bin = precision_score(y_true, y_pred)
     f1 = f1_score(y_true, y_pred)
-    recall = recall_score(y_true, y_pred)
-    print(roc_auc , precision, recall, f1)
+    recall_bin = recall_score(y_true, y_pred)
+
+    print("roc:", roc_auc, "aupr:", aupr)
+
+
     cm = confusion_matrix(y_true, y_pred)
-    cm_1 = cm[0][0]
-    cm_2 = cm[0][1]
-    cm_3 = cm[1][0]
-    cm_4 = cm[1][1]
-    false_positives = (y_pred == 1) & (y_true == 0)
-    fp_indices = idx_test.cpu()[false_positives].numpy()
-    print(cm)
-    return acc_test.item() , roc_auc.item(), precision.item(), recall.item(),f1.item(),cm_1.item(),cm_2.item(),cm_3.item(),cm_4.item() , aupr.item()
+    cm_1, cm_2, cm_3, cm_4 = cm.ravel()
+
+
+    return (
+        acc_test.item(), roc_auc.item(), precision_bin.item(), recall_bin.item(),
+        f1.item(), cm_1.item(), cm_2.item(), cm_3.item(), cm_4.item(),
+        aupr.item()
+    )
     
 def accuracy(output, labels , roc ):
     preds = (output > roc).type_as(labels)
@@ -322,12 +352,19 @@ def get_split3(data_y , p):
 
 import glob
 import os
-lr_list = [ 0.01, 0.005 ]
-dp_list = [ 0.6 ]
-heads_list = [8, 4, 2, 1 ]
-hid_list = [ 64  , 32  , 16 , 8]
-wd_list = [ 0.001, 0.0005 ]
-positional_list = [ 64 ,32 , 16  , 8]
+# lr_list = [ 0.01, 0.005 ]
+# dp_list = [ 0.6 ]
+# heads_list = [8, 4, 2, 1 ]
+# hid_list = [ 64 , 32  , 16 , 8]
+# wd_list = [ 0.001, 0.0005 ]
+# geneflow_list = [ 64 ,32 , 16  , 8]
+
+lr_list = [ 0.01 ]
+dp_list = [ 0.2 ]
+heads_list = [ 1 ]
+hid_list = [  64]
+wd_list = [ 0.001 ]
+geneflow_list = [32]
 
 hyper_list = []
     
@@ -336,8 +373,8 @@ for nb_heads in heads_list:
         for lr in lr_list:
             for dropout in dp_list:
                 for weight_decay in wd_list:
-                    for positional_hidden in positional_list:
-                        hyper_list.append( [ nb_heads , hidden , lr , dropout , weight_decay , positional_hidden])
+                    for geneflow_hidden in geneflow_list:
+                        hyper_list.append( [ nb_heads , hidden , lr , dropout , weight_decay , geneflow_hidden])
 
 for idd in range( 0 , len(hyper_list)):
     all_acc=[]
@@ -349,7 +386,7 @@ for idd in range( 0 , len(hyper_list)):
         alpha = 0.2
         lr = hyper_list[idd][2]
         weight_decay = hyper_list[idd][4]
-        positional_hidden = hyper_list[idd][5]
+        geneflow_hidden = hyper_list[idd][5]
         epochs = 1000
         patience = 100
         t_total = time.time()
@@ -359,8 +396,8 @@ for idd in range( 0 , len(hyper_list)):
         best_epoch = 0
         edge_index = torch.tensor(np.loadtxt('data/edge').T)
         edge_index = edge_index.to(torch.int)
-        data = np.load('data/one_hot_array.npy')
-        data = np.transpose(data,(1,0,2))
+        # sequence features (available via GitHub Release)
+        data = np.load('data/sequence.npy')
         features  =  torch.tensor(data)
         labels =  np.loadtxt('data/virus_lab')
         labels = torch.tensor(labels.astype(int))
@@ -377,7 +414,7 @@ for idd in range( 0 , len(hyper_list)):
                         dropout=dropout, 
                         nheads=nb_heads, 
                         alpha=alpha,
-                        positional_hidden = positional_hidden
+                        geneflow_hidden = geneflow_hidden
                         )
         optimizer = optim.Adam(model.parameters(), 
                                     lr=lr, 
@@ -421,26 +458,16 @@ for idd in range( 0 , len(hyper_list)):
         model.load_state_dict(torch.load('{}.pkl'.format(best_epoch)))
         result = compute_test()
         all_acc.append(result)
-        formatted_acc = int(np.array(result)[1] * 10000)
         files = glob.glob('*.pkl')
         os.remove(files[0])
     all_acc = np.array(all_acc)
     with open("result.txt","a") as f:      
-        f.write("\n"+str(idd)+ ":  "+str(all_acc[:,0].mean()) +"  "+str(all_acc[:,1].mean())+ "  "
-                +str(all_acc[:,2].mean())+ "  "+str(all_acc[:,3].mean())+ "  "+str(all_acc[:,4].mean())+"  "+
-                str(all_acc[:,5].mean())+"  "+
-                str(all_acc[:,6].mean())+"  "+
-                str(all_acc[:,7].mean())+"  "+
-                str(all_acc[:,8].mean())+"  "+
-                str(all_acc[:,9].mean())+"  "+
-                str(all_acc[:,0].std())+"  "+
-                str(all_acc[:,1].std())+"  "+
-                str(all_acc[:,2].std())+"  "+
-                str(all_acc[:,3].std())+"  "+
-                str(all_acc[:,4].std())+"  "+
-                str(all_acc[:,9].std())+"  "+
-            "    heads:"+ str(nb_heads) +
-            "    hidden: "+ str(hidden)+
-            "    dropout: "+str(dropout)+
-            "    lr:"+str(lr)+
-            "    weight_decay:"+str(weight_decay) )
+        f.write("\n" + str(idd) + ":\n")
+        f.write(f"  aupr: {all_acc[:,9].mean():.4f} ± {all_acc[:,9].std():.4f} \n")
+        f.write(f"  auc: {all_acc[:,1].mean():.4f} ± {all_acc[:,1].std():.4f} \n")
+        f.write(f"  acc: {all_acc[:,0].mean():.4f} ± {all_acc[:,0].std():.4f} \n")
+        f.write(f"  precision: {all_acc[:,2].mean():.4f} ± {all_acc[:,2].std():.4f} \n")
+        f.write(f"  recall: {all_acc[:,3].mean():.4f} ± {all_acc[:,3].std():.4f}\n")
+        f.write(f"  f1: {all_acc[:,4].mean():.4f} ± {all_acc[:,4].std():.4f} \n")
+        f.write(f"    heads: {nb_heads}  hidden: {hidden}  dropout: {dropout}  "
+                f"lr: {lr}  weight_decay: {weight_decay}\n")
